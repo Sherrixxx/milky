3df48fe985939bf47e536de5a5b13143e2c53b08
Sherrixxx <84737310+Sherrixxx@users.noreply.github.com>
2021-05-27 17:38:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,8 +45,8 @@ async def credits( ctx ):
 async def ударить(ctx, member: discord.Member = None):
     if member is None:
         return await ctx.send("Ты что, воздух ударил?")
-    embed=discord.Embed(title="Удар", description = "{0} ударил {1}#{2}.".format(ctx.author, member.name, member.discriminator), color=0xfff700)
-    embed.set_thumbnail(url="https://img.gifmagazine.net/gifmagazine/images/656808/original.gif")
+    embed=discord.Embed(title="Удар", description = "{0.mention} ударил {1}#{2}.".format(ctx.author, member.name, member.discriminator), color=0xfff700)
+    embed.set_image(url="https://img.gifmagazine.net/gifmagazine/images/656808/original.gif")
     await ctx.send(embed=embed)
 
 token = os.environ.get('BOT_TOKEN')
