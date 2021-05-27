ec17f5b3f382653b06b6078848fbcc6fa85f230c
Sherrixxx <84737310+Sherrixxx@users.noreply.github.com>
2021-05-27 18:18:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ client.remove_command('help')
 cluster = MongoClient("mongodb+srv://morgenshtern:tupaparolotbotaklassno@gg-wp.tltaz.mongodb.net/morgenshtern?retryWrites=true&w=majority")
 collection = cluster.ecodb.colldb
 
+gifs_punch=["https://img.gifmagazine.net/gifmagazine/images/656808/original.gif", "гифка 2"]
+    ударить=random.choice(gifs_punch)
+    await ctx.send(ударить)
+
 @client.event
 async def on_ready():
     print('Зарегистрирован в {0}.'.format(client.user))
@@ -43,9 +47,6 @@ async def credits( ctx ):
 @client.command( pass_context = True )
 
 async def ударить(ctx, member: discord.Member = None):
-    gifs_punch=["https://img.gifmagazine.net/gifmagazine/images/656808/original.gif", "гифка 2"]
-    ударить=random.choice(gifs_punch)
-    await ctx.send(ударить)
     if member is None:
         return await ctx.send("Ты что, воздух ударил?")
     embed=discord.Embed(title="Удар", description = "{0.mention} ударил {1}.".format(ctx.author, member.mention), color=0xfff700)
